a420be44efd6dbc2aa33ffa2f6cfe1c79134d5f1
cihp <itsjustchipoverhere@proton.me>
2025-06-29 17:56:49 +0700

    GOONING SIMULATOR

---
+++ gooning simulator.py
@@ -0,0 +1,26 @@
+import gi
+gi.require_version('Gtk', '4.0')
+
+from gi.repository import Gtk
+class page(Gtk.ApplicationWindow):
+    def __init__(self, **kargs):
+        super().__init__(**kargs, title="gooning simulator")
+        self.button = Gtk.Button(label='Click Here')
+        self.button.connect('clicked', self.on_button_clicked)
+        self.set_child(self.button)
+    
+    def on_button_clicked(self, _widget):
+        goon += 1
+        print('goon-ed')
+        print(f'Total goon times : {goon}')
+
+def on_activate(app):
+    win = page(application=app)
+    win.present()
+
+
+app = Gtk.Application(application_id='com.example.App')
+app.connect('activate', on_activate)
+app.run(None)
+
+# joke pls dont kill me
